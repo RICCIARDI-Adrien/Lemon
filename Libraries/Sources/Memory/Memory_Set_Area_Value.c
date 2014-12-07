cd3b6cbd7609fe3ada12108887c5da2eb07ade6a
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2014-12-07 14:00:09 +0100

    Optimized MemorySetAreaValue() speed.

--- Libraries/Sources/Memory/Memory_Set_Area_Value.c
+++ Libraries/Sources/Memory/Memory_Set_Area_Value.c
@@ -9,14 +9,25 @@
 //-------------------------------------------------------------------------------------------------
 void MemorySetAreaValue(void *Pointer_Area, unsigned int Bytes_Count, unsigned char Value)
 {
-	unsigned char *Pointer_Area_Bytes;
+	unsigned int *Pointer_Area_Dword, Value_Dword;
+	unsigned char *Pointer_Area_Byte;
 	
-	Pointer_Area_Bytes = Pointer_Area;
+	// Set the value by 4 bytes at a time to be faster
+	Value_Dword = (Value << 24) | (Value << 16) | (Value << 8) | Value; // Extend the value on 32 bits
+	Pointer_Area_Dword = Pointer_Area;
+	while (Bytes_Count > sizeof(unsigned int))
+	{
+		*Pointer_Area_Dword = Value_Dword;
+		Pointer_Area_Dword++;
+		Bytes_Count -= sizeof(unsigned int);
+	}
 	
+	// Set the value of the remaining bytes
+	Pointer_Area_Byte = (unsigned char *) Pointer_Area_Dword;
 	while (Bytes_Count > 0)
 	{
-		*Pointer_Area_Bytes = Value;
-		Pointer_Area_Bytes++;
+		*Pointer_Area_Byte = Value;
+		Pointer_Area_Byte++;
 		Bytes_Count--;
 	}
 }
