1a781388399c364e30a8de015db168dadd27d5e1
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2016-04-01 23:54:28 +0200

    Added word and double-word functions for Intel I/O space access.

--- System/Includes/Hardware_Functions.h
+++ System/Includes/Hardware_Functions.h
@@ -10,14 +10,38 @@
 //-------------------------------------------------------------------------------------------------
 /** Write a byte to the specified port on the system bus.
  * @param Port The port number.
- * @param Byte The byte to write.
+ * @param Data The data to write.
  */
-void outb(unsigned short Port, unsigned char Byte);
+void outb(unsigned short Port, unsigned char Data);
+
+/** Write a word to the specified port on the system bus.
+ * @param Port The port number.
+ * @param Data The data to write.
+ */
+void outw(unsigned short Port, unsigned short Data);
+
+/** Write a double-word to the specified port on the system bus.
+ * @param Port The port number.
+ * @param Data The data to write.
+ */
+void outd(unsigned short Port, unsigned int Data);
 
 /** Read a byte from the system bus.
- * @param Port The port number from which read the byte.
- * @return The byte read.
+ * @param Port The port number to read from.
+ * @return The read byte.
  */
 unsigned char inb(unsigned short Port);
+
+/** Read a word from the system bus.
+ * @param Port The port number to read from.
+ * @return The read word.
+ */
+unsigned short inw(unsigned short Port);
+
+/** Read a double-word from the system bus.
+ * @param Port The port number to read from.
+ * @return The read double-word.
+ */
+unsigned int ind(unsigned short Port);
 
 #endif
--- System/Sources/Hardware_Functions.asm
+++ System/Sources/Hardware_Functions.asm
@@ -3,19 +3,53 @@
 [BITS 32]
 
 GLOBAL outb
+GLOBAL outw
+GLOBAL outd
 GLOBAL inb
+GLOBAL inw
+GLOBAL ind
 
 outb:
 	push edx
-	mov edx,[esp+8]
-	mov eax,[esp+12]
-	out dx,al
+	mov edx, [esp + 8]
+	mov eax, [esp + 12]
+	out dx, al
+	pop edx
+	ret
+	
+outw:
+	push edx
+	mov edx, [esp + 8]
+	mov eax, [esp + 12]
+	out dx, ax
+	pop edx
+	ret
+	
+outd:
+	push edx
+	mov edx, [esp + 8]
+	mov eax, [esp + 12]
+	out dx, eax
 	pop edx
 	ret
 	
 inb:
 	push edx
-	mov edx,[esp+8]
-	in al,dx
+	mov edx, [esp + 8]
+	in al, dx
+	pop edx
+	ret
+	
+inw:
+	push edx
+	mov edx, [esp + 8]
+	in ax, dx
+	pop edx
+	ret
+	
+ind:
+	push edx
+	mov edx, [esp + 8]
+	in eax, dx
 	pop edx
 	ret
