a3eee50fe55b64f795bcf59b57d8e38d0ecfc19f
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2016-09-08 22:02:35 +0200

    Reworked Benchmark application to add multiple benchmark tests (only processor speed benchmark is currently available).

--- Applications/Sources/Benchmark/Benchmark.c
+++ Applications/Sources/Benchmark/Benchmark.c
@@ -1,7 +1,6 @@
 /** @file Benchmark.c
- * Compute the Fibonacci suite up to the requested numbers of iterations in order to stress the processor.
+ * Execute several benchmarks to determine system speed and test its stability.
  * @author Adrien RICCIARDI
- * @version 1.0 : 03/01/2015
  */
 #include <System.h>
 
@@ -9,50 +8,103 @@
 // Private constants
 //-------------------------------------------------------------------------------------------------
 /** How many iterations to compute. */
-#define ITERATIONS_COUNT 10000000
+#define BENCHMARK_PROCESSOR_ITERATIONS_COUNT 10000000
 
 /** Convert the macro identifier to a C string. */
-#define CONVERT_MACRO_NAME_TO_STRING(X) #X
-/** Convert the macro value to a C string. The preprocessor needs two passes to do the conversion, so the CONVERT_MACRO_NAME_TO_STRING() is needed. */
-#define CONVERT_MACRO_VALUE_TO_STRING(X) CONVERT_MACRO_NAME_TO_STRING(X)
+#define BENCHMARK_CONVERT_MACRO_NAME_TO_STRING(X) #X
+/** Convert the macro value to a C string. The preprocessor needs two passes to do the conversion, so the BENCHMARK_CONVERT_MACRO_NAME_TO_STRING() is needed. */
+#define BENCHMARK_CONVERT_MACRO_VALUE_TO_STRING(X) BENCHMARK_CONVERT_MACRO_NAME_TO_STRING(X)
 
 //-------------------------------------------------------------------------------------------------
-// Entry point
+// Private types
 //-------------------------------------------------------------------------------------------------
-int main(void)
+/** A benchmark callback.
+ * @param Pointer_Start_Time On output, contain the benchmark starting time in milliseconds.
+ * @param Pointer_End_Time On output, contain the benchmark ending time in milliseconds.
+ */
+typedef void (*BenchmarkFunction)(unsigned int *Pointer_Start_Time, unsigned int *Pointer_End_Time);
+
+/** A benchmark test. */
+typedef struct
+{
+	char String_Tested_Subsystem[64]; //! What subsystem the benchmark is testing.
+	char String_Description[256]; //! What the benchmark does.
+	BenchmarkFunction Function; //! The test code.
+} TBenchmarkTest;
+
+//-------------------------------------------------------------------------------------------------
+// Prototypes
+//-------------------------------------------------------------------------------------------------
+static void BenchmarkProcessor(unsigned int *Pointer_Start_Time, unsigned int *Pointer_End_Time);
+
+//-------------------------------------------------------------------------------------------------
+// Private variables
+//-------------------------------------------------------------------------------------------------
+/** All available benchmarks. */
+static TBenchmarkTest Benchmark_Tests[] =
+{
+	{
+		"processor speed",
+		"Computing Fibonacci suite up to " BENCHMARK_CONVERT_MACRO_VALUE_TO_STRING(BENCHMARK_PROCESSOR_ITERATIONS_COUNT) " iterations with 32768 bits integer\nprecision",
+		BenchmarkProcessor
+	},
+	// TODO
+};
+
+//-------------------------------------------------------------------------------------------------
+// Private functions
+//-------------------------------------------------------------------------------------------------
+/** Compute the Fibonacci suite up to the requested numbers of iterations in order to stress the processor. */
+static void BenchmarkProcessor(unsigned int *Pointer_Start_Time, unsigned int *Pointer_End_Time)
 {
 	static TMathInteger Penultimate_Number, Last_Number, Result; // Do not store these variables on the stack as they are a bit heavy
 	unsigned long long Iterations;
-	unsigned int Start_Time, End_Time;
-	
-	ScreenWriteString("Starting computation.\n");
-	ScreenWriteString("Iterations count : ");
-	ScreenWriteString(CONVERT_MACRO_VALUE_TO_STRING(ITERATIONS_COUNT));
-	ScreenWriteString("\nInteger precision : 32768 bits\n");
-	Start_Time = SystemGetTimerValue();
 	
 	// Initialize the first numbers
 	MathIntegerInitializeFromInt(0, &Penultimate_Number);
 	MathIntegerInitializeFromInt(1, &Last_Number);
 	
-	for (Iterations = 0; Iterations < ITERATIONS_COUNT; Iterations++)
+	*Pointer_Start_Time = SystemGetTimerValue();
+	
+	for (Iterations = 0; Iterations < BENCHMARK_PROCESSOR_ITERATIONS_COUNT; Iterations++)
 	{
 		MathIntegerAdd(&Penultimate_Number, &Last_Number, &Result);
 		MathIntegerCopy(&Last_Number, &Penultimate_Number);
 		MathIntegerCopy(&Result, &Last_Number);
 	}
 	
-	End_Time = SystemGetTimerValue();
+	*Pointer_End_Time = SystemGetTimerValue();
+}
+
+//-------------------------------------------------------------------------------------------------
+// Entry point
+//-------------------------------------------------------------------------------------------------
+int main(void)
+{
+	unsigned int Start_Time, End_Time, i;
 	
-	ScreenWriteString("Result :\n");
-	MathIntegerDisplayHexadecimal(&Result);
-	ScreenWriteCharacter('\n');
-	
-	ScreenWriteString("Computation duration (+/- 1 second) : ");
-	ScreenSetFontColor(SCREEN_COLOR_GREEN);
-	ScreenWriteUnsignedInteger((End_Time - Start_Time) / 1000); // Convert to seconds
-	ScreenSetFontColor(SCREEN_COLOR_BLUE);
-	ScreenWriteString(" seconds.\n");
+	// Execute all benchmarks
+	for (i = 0; i < sizeof(Benchmark_Tests) / sizeof(TBenchmarkTest); i++)
+	{
+		// Display benchmark information
+		ScreenWriteString("### Evaluating ");
+		ScreenSetFontColor(SCREEN_COLOR_GREEN);
+		ScreenWriteString(Benchmark_Tests[i].String_Tested_Subsystem);
+		ScreenSetFontColor(SCREEN_COLOR_BLUE);
+		ScreenWriteString(" ###\n");
+		ScreenWriteString(Benchmark_Tests[i].String_Description);
+		ScreenWriteString(".\n");
+		
+		// Execute it
+		Benchmark_Tests[i].Function(&Start_Time, &End_Time);
+		
+		// Display elapsed time
+		ScreenWriteString("Benchmark duration : ");
+		ScreenSetFontColor(SCREEN_COLOR_GREEN);
+		ScreenWriteUnsignedInteger(End_Time - Start_Time);
+		ScreenSetFontColor(SCREEN_COLOR_BLUE);
+		ScreenWriteString(" milliseconds.\n\n");
+	}
 	
 	return 0;
 }
