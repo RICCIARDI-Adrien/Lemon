9a2d6a1d593d126e54bcce5d8bdcf3405460dec5
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2016-10-23 11:23:58 +0200

    Fixed Realtek 8111 ethernet driver PCI registers bad alignment.

--- System/Sources/Drivers/Driver_Ethernet_Realtek_8111.c
+++ System/Sources/Drivers/Driver_Ethernet_Realtek_8111.c
@@ -10,8 +10,6 @@
 //-------------------------------------------------------------------------------------------------
 // Private constants
 //-------------------------------------------------------------------------------------------------
-/** Reset the whole controller when set to 1 (this bit is self-cleared when the reset is finished). */
-#define ETHERNET_CONTROLLER_COMMAND_REGISTER_RESET_BIT 4
 /** Enable reception when set to 1. */
 #define ETHERNET_CONTROLLER_COMMAND_REGISTER_RECEIVER_ENABLE_BIT 3
 /** Enable transmission when set to 1. */
@@ -35,13 +33,13 @@
 #define ETHERNET_CONTROLLER_PHY_STATUS_REGISTER_LINK_FULL_DUPLEX_STATUS_BIT 0
 
 /** Set to 1 to start transmitting data. The bit is automatically cleared when data has been transmitted. */
-#define ETHERNET_CONTROLLER_TRANSMIT_DESCRIPTOR_OWNERSHIP_BIT 31
+#define ETHERNET_CONTROLLER_TRANSMIT_DESCRIPTOR_OWNERSHIP_BIT 15
 /** Set to 1 to tell that this is the last descriptor in the descriptors ring. */
-#define ETHERNET_CONTROLLER_TRANSMIT_DESCRIPTOR_END_OF_DESCRIPTOR_RING_BIT 30
+#define ETHERNET_CONTROLLER_TRANSMIT_DESCRIPTOR_END_OF_DESCRIPTOR_RING_BIT 14
 /** Tell that the descriptor contains the packet beginning. */
-#define ETHERNET_CONTROLLER_TRANSMIT_DESCRIPTOR_FIRST_SEGMENT_DESCRIPTOR_BIT 29
+#define ETHERNET_CONTROLLER_TRANSMIT_DESCRIPTOR_FIRST_SEGMENT_DESCRIPTOR_BIT 13
 /** Tell that the descriptor contains the packet end. */
-#define ETHERNET_CONTROLLER_TRANSMIT_DESCRIPTOR_LAST_SEGMENT_DESCRIPTOR_BIT 28
+#define ETHERNET_CONTROLLER_TRANSMIT_DESCRIPTOR_LAST_SEGMENT_DESCRIPTOR_BIT 12
 
 //-------------------------------------------------------------------------------------------------
 // Private types
@@ -51,49 +49,63 @@ typedef struct __attribute__((packed))
 {
 	// Offset 0
 	unsigned char ID_Registers[6]; //!< Hold the MAC address loaded from the EEPROM (warning, this field must be written using double-word accesses).
-		
-	unsigned char Reserved_1[0x37 - 6];
 	
-	/*unsigned int ID_Registers_Low;
-	unsigned int ID_Registers_High;
-		
-	unsigned char Reserved_1[0x37 - 8];*/
+	unsigned char Reserved_1[0x20 - 6];
 	
+	// Offset 0x20
+	volatile void *Pointer_Transmit_Normal_Priority_Descriptors_High;
+	// Offset 0x24
+	volatile void *Pointer_Transmit_Normal_Priority_Descriptors_Low;
+	
+	unsigned char Reserved_2[0x37 - 0x28];
+		
 	// Offset 0x37
 	unsigned char Command;
 	// Offset 0x38
 	unsigned char Transmit_Priority_Polling; //!< Tell if there is a packet to send (in high or in normal priority rings).
-	
-	unsigned char Reserved_2[0x3C - 2];
-	
+	unsigned char Reserved_3[3];
 	// Offset 0x3C
 	unsigned short Interrupt_Mask;
 	// Offset 0x3E
 	unsigned short Interrupt_Status;
-	
-	unsigned char Reserved_3[0x44 - 4];
-	
+	// Offset 0x40
+	unsigned int Transmit_Configuration;
 	// Offset 0x44
 	unsigned int Receive_Configuration;
 	
-	unsigned char Reserved_4[0x6C - 4];
+	unsigned char Reserved_5[0x60 - 0x48];
+	
+	// Offset 0x60
+	unsigned int PHY_Access;
+	
+	unsigned char Reserved_6[0x6C - 0x64];
 	
 	// Offset 0x6C
 	unsigned char PHY_Status; //!< Reflect link status (speed, duplex mode, state...).
 	
-	unsigned char Reserved_5[0xDA - 1];
+	unsigned char Reserved_7[0xDA - 0x6D];
 	
 	// Offset 0xDA
-	unsigned int Receive_Packet_Maximum_Size;
+	unsigned short Receive_Packet_Maximum_Size;
 	
-	unsigned char Reserved_6[0xE4 - 4];
+	unsigned char Reserved_8[0xE0 - 0xDC];
 	
+	// Offset 0xE0
+	unsigned char C_Plus_Command;
+	unsigned char Reserved_9[3];
 	// Offset 0xE4
 	volatile void *Pointer_Receive_Descriptor_Start_Address_High;
 	// Offset 0xE8
 	volatile void *Pointer_Receive_Descriptor_Start_Address_Low;
-	
+	// Offset 0xEC
+	unsigned char Maximum_Transmit_Packet_Size; //!< Only bits 5..0 are used. The value is in 128-byte units.
 } TEthernetControllerRegisters;
+
+typedef enum
+{
+	ETHERNET_CONTROLLER_PHY_REGISTER_ADDRESS_BMCR,
+	
+} TEthernetControllerPHYRegisterAddress;
 
 /** A receive descriptor. */
 typedef struct __attribute__((packed))
@@ -123,9 +135,43 @@ static TEthernetControllerRegisters *Pointer_Ethernet_Controller_Registers;
 
 /** The receive buffer descriptors list (currently limited to only one descriptor). */
 static volatile TEthernetControllerReceiveDescriptor __attribute__((aligned(256))) Ethernet_Controller_Receive_Descriptor; // Must be 256-byte aligned
+/** The transmit buffer descriptors list (currently limited to only one descriptor). */
+static volatile TEthernetControllerTransmitDescriptor __attribute__((aligned(256))) Ethernet_Controller_Transmit_Descriptor; // Must be 256-byte aligned
 
 /** The buffer dedicated to packets reception. */
 static unsigned char Ethernet_Controller_Reception_Buffer[CONFIGURATION_ETHERNET_BUFFER_SIZE];
+/** The buffer dedicated to packets transmission. */
+static unsigned char Ethernet_Controller_Transmission_Buffer[CONFIGURATION_ETHERNET_BUFFER_SIZE];
+
+//-------------------------------------------------------------------------------------------------
+// Private functions
+//-------------------------------------------------------------------------------------------------
+/** TODO */
+#if 0
+static void EthernetControllerPHYWrite(TEthernetControllerPHYRegisterAddress Address, unsigned short Data)
+{
+	unsigned int Register_Content, jabba;
+	
+	// Prepare the register content
+	Register_Content = (Address << 16) & 0x001F0000; // Set the MII register address
+	Register_Content |= Data; // Set the MII register data
+	Register_Content |= 1 << 31; // Tell this is a write operation
+	
+	// Start write operation
+	Pointer_Ethernet_Controller_Registers->PHY_Access = Register_Content;
+	jabba = Pointer_Ethernet_Controller_Registers->PHY_Access;
+	
+	ScreenWriteString("Register_Content : "); ScreenWriteString(itoa(Register_Content)); ScreenWriteString("\n");
+	ScreenWriteString("PHY_Access : "); ScreenWriteString(itoa(jabba)); ScreenWriteString("\n");
+	KeyboardReadCharacter();
+	
+	// Wait for the operation to terminate
+	while (Pointer_Ethernet_Controller_Registers->PHY_Access & (1 << 31))
+	{
+		ScreenWriteString("read : "); ScreenWriteString(itoa(Pointer_Ethernet_Controller_Registers->PHY_Access)); ScreenWriteString("\n");
+	}
+}
+#endif
 
 //-------------------------------------------------------------------------------------------------
 // Public functions
@@ -134,7 +180,7 @@ int EthernetInitialize(void)
 {
 	TPCIDeviceID Device_ID;
 	TPCIConfigurationSpaceHeader Configuration_Space_Header;
-	unsigned int i;//, MAC_Address_Double_Words[2];
+	unsigned int i;
 	
 	// Try to find an ethernet controller (can't directly use the device ID to find the device as the device ID is programmed by the board manufacturer with an unknown value)
 	if (PCIFindDeviceFromClass(PCI_CLASS_CODE_BASE_NETWORK_CONTROLLER, PCI_CLASS_CODE_SUB_CLASS_ETHERNET, &Device_ID) != 0) return 1;
@@ -158,22 +204,10 @@ int EthernetInitialize(void)
 	// Give access to registers
 	Pointer_Ethernet_Controller_Registers = (TEthernetControllerRegisters *) PCI_GET_BASE_ADDRESS(Configuration_Space_Header.Base_Address_Registers[2]);
 	
-	// Reset the controller
-	/*DEBUG_SECTION_START
-	DEBUG_DISPLAY_CURRENT_FUNCTION_NAME();
-	{
-		ScreenWriteString("Resetting ethernet controller.\n");
-		KeyboardReadCharacter();
-	}
-	DEBUG_SECTION_END
-	
-	Pointer_Ethernet_Controller_Registers->Command = 1 << ETHERNET_CONTROLLER_COMMAND_REGISTER_RESET_BIT;
-	while (Pointer_Ethernet_Controller_Registers->Command & (1 << ETHERNET_CONTROLLER_COMMAND_REGISTER_RESET_BIT)); // Wait for the reset to terminate
-	*/
-	
-	// Enable link auto-negotiation
-	
-	
+	// Disable some offloading features (this register should be the first configuration step according to datasheet)
+	Pointer_Ethernet_Controller_Registers->C_Plus_Command = 0;
+	// Disable reception and transmission until the controller is fully configured (this register should be the second configuration step according to datasheet)
+	Pointer_Ethernet_Controller_Registers->Command = 0;
 	
 	//===============================================
 	// Reception configuration
@@ -182,10 +216,11 @@ int EthernetInitialize(void)
 	Pointer_Ethernet_Controller_Registers->Receive_Packet_Maximum_Size = CONFIGURATION_ETHERNET_BUFFER_SIZE;
 	
 	// Configure the reception behavior
+	Pointer_Ethernet_Controller_Registers->Command |= 1 << ETHERNET_CONTROLLER_COMMAND_REGISTER_RECEIVER_ENABLE_BIT; // Enable reception in case the reception register would have the same access needs than the transmission register
 	Pointer_Ethernet_Controller_Registers->Receive_Configuration = (0x07 << 13) | (0x07 << 8) | (1 << ETHERNET_CONTROLLER_RECEIVE_CONFIGURATION_REGISTER_ACCEPT_BROADCAST_PACKETS_BIT); // Copy a received packet to host memory when the packet has been fully received, set DMA burst size to unlimited (TODO : why ?), accept broadcast packets, discard erroneous packets
 	
-	// Initialize the only reception descriptor
-	Ethernet_Controller_Receive_Descriptor.Flags = 0; // The descriptor is owned by the system
+	// Initialize the single reception descriptor
+	Ethernet_Controller_Receive_Descriptor.Flags = 0; // Make the descriptor be owned by the system
 	Ethernet_Controller_Receive_Descriptor.Pointer_Buffer_Address_High = NULL;
 	Ethernet_Controller_Receive_Descriptor.Pointer_Buffer_Address_Low = Ethernet_Controller_Reception_Buffer;
 	
@@ -196,27 +231,35 @@ int EthernetInitialize(void)
 	//===============================================
 	// Transmission configuration
 	//===============================================
+	// Set the maximum allowed packet size for transmission
+	Pointer_Ethernet_Controller_Registers->Maximum_Transmit_Packet_Size = CONFIGURATION_ETHERNET_BUFFER_SIZE / 128; // The value is in 128-byte unit, take care of rounding...
 	
+	// Configure the transmission behavior
+	Pointer_Ethernet_Controller_Registers->Command |= 1 << ETHERNET_CONTROLLER_COMMAND_REGISTER_TRANSMITTER_ENABLE_BIT; // Transmission must be enabled to allow the transmission register usage
+	Pointer_Ethernet_Controller_Registers->Transmit_Configuration = (0x03 << 24) | (0 << 19) | (0x07 << 8); // Set default inter-frame gap time, set normal mode, set unlimited DMA burst size
+	
+	// Initialize the single transmission descriptor
+	Ethernet_Controller_Transmit_Descriptor.Flags = 0; // Make the descriptor be owned by the system
+	Ethernet_Controller_Transmit_Descriptor.Pointer_Buffer_Address_High = NULL;
+	Ethernet_Controller_Transmit_Descriptor.Pointer_Buffer_Address_Low = &Ethernet_Controller_Transmission_Buffer;
+	
+	// Set the transmission buffers ring
+	Pointer_Ethernet_Controller_Registers->Pointer_Transmit_Normal_Priority_Descriptors_High = NULL;
+	Pointer_Ethernet_Controller_Registers->Pointer_Transmit_Normal_Priority_Descriptors_Low = &Ethernet_Controller_Transmit_Descriptor;
 	
 	// Mask all interrupts
 	Pointer_Ethernet_Controller_Registers->Interrupt_Mask = 0;
 	
-	// The controller MAC address is loaded from the board EEPROM, no need to set it. Get it at the initialization end to make sure the field was not overwritten by previous initialization
-	// Get the MAC address (this PCI registers must be read using 32-bit accesses)
-	/*MAC_Address_Double_Words[0] = Pointer_Ethernet_Controller_Registers->ID_Registers_Low;
-	MAC_Address_Double_Words[1] = Pointer_Ethernet_Controller_Registers->ID_Registers_High;
-	
-	// Put bytes in displayable order
-	Ethernet_Controller_MAC_Address[0] = (unsigned char) MAC_Address_Double_Words[0];
-	Ethernet_Controller_MAC_Address[1] = MAC_Address_Double_Words[0] >> 8;
-	Ethernet_Controller_MAC_Address[2] = MAC_Address_Double_Words[0] >> 16;
-	Ethernet_Controller_MAC_Address[3] = MAC_Address_Double_Words[0] >> 24;
-	Ethernet_Controller_MAC_Address[4] = (unsigned char) MAC_Address_Double_Words[1];
-	Ethernet_Controller_MAC_Address[5] = MAC_Address_Double_Words[1] >> 8;*/
+	// Get MAC address
 	for (i = 0; i < sizeof(Ethernet_Controller_MAC_Address); i++) Ethernet_Controller_MAC_Address[i] = Pointer_Ethernet_Controller_Registers->ID_Registers[i];
 	
-	// Enable transmission and reception
-	//Pointer_Ethernet_Controller_Registers->Command |= (1 << ETHERNET_CONTROLLER_COMMAND_REGISTER_RECEIVER_ENABLE_BIT) | (1 << ETHERNET_CONTROLLER_COMMAND_REGISTER_TRANSMITTER_ENABLE_BIT);
+	//===============================================
+	// PHY configuration
+	//===============================================
+	// Enable link auto-negotiation, force it again and put PHY in normal working mode
+	//EthernetControllerPHYWrite(ETHERNET_CONTROLLER_PHY_REGISTER_ADDRESS_BMCR, 0x1200);
+	
+	//KeyboardReadCharacter();
 	
 	DEBUG_SECTION_START
 	DEBUG_DISPLAY_CURRENT_FUNCTION_NAME();
@@ -252,7 +295,7 @@ int EthernetInitialize(void)
 		else String_Text = "1000";
 		ScreenWriteString(String_Text);
 		ScreenWriteString(" Mb/s\n");
-		
+			
 		KeyboardReadCharacter();
 	}
 	DEBUG_SECTION_END
@@ -263,14 +306,27 @@ int EthernetInitialize(void)
 void EthernetReceivePacket(unsigned int __attribute__((unused)) *Pointer_Buffer_Size, void __attribute__((unused)) *Pointer_Buffer)
 {
 	// TODO
+	ScreenWriteString("+++ EthernetReceivePacket +++\n");
 }
 
-void EthernetSendPacket(unsigned int __attribute__((unused)) Buffer_Size, void __attribute__((unused)) *Pointer_Buffer)
+void EthernetSendPacket(unsigned int Buffer_Size, void *Pointer_Buffer)
 {
-	// TODO
+	// Set the source MAC address
+	memcpy(Pointer_Buffer + ETHERNET_CONTROLLER_MAC_ADDRESS_SIZE, Ethernet_Controller_MAC_Address, ETHERNET_CONTROLLER_MAC_ADDRESS_SIZE);
 	
-	// Tell that a packet is available to send
-	// Pointer_Ethernet_Controller_Registers->Transmit_Priority_Polling |= ETHERNET_CONTROLLER_TRANSMIT_PRIORITY_POLLING_REGISTER_NORMAL_PRIORITY_QUEUE_POLLING_BIT;
+	// Copy the packet content to the ethernet controller transmission buffer
+	if (Buffer_Size > CONFIGURATION_ETHERNET_BUFFER_SIZE) Buffer_Size = CONFIGURATION_ETHERNET_BUFFER_SIZE; // Make sure the packet is not too big for the destination buffer
+	memcpy(Ethernet_Controller_Transmission_Buffer, Pointer_Buffer, Buffer_Size);
+	
+	// Configure the transmission descriptor
+	Ethernet_Controller_Transmit_Descriptor.Frame_Length = Buffer_Size;
+	Ethernet_Controller_Transmit_Descriptor.Flags = (1 << ETHERNET_CONTROLLER_TRANSMIT_DESCRIPTOR_OWNERSHIP_BIT) | (1 << ETHERNET_CONTROLLER_TRANSMIT_DESCRIPTOR_END_OF_DESCRIPTOR_RING_BIT) | (1 << ETHERNET_CONTROLLER_TRANSMIT_DESCRIPTOR_FIRST_SEGMENT_DESCRIPTOR_BIT) | (1 << ETHERNET_CONTROLLER_TRANSMIT_DESCRIPTOR_LAST_SEGMENT_DESCRIPTOR_BIT); // Give the buffer ownership to the network controller, tell this descriptor is last one of the descriptors ring so the controller will restart from the ring beginning next time, tell that the packet is fully contained in this buffer
+	
+	// Send the packet
+	Pointer_Ethernet_Controller_Registers->Transmit_Priority_Polling |= 1 << ETHERNET_CONTROLLER_TRANSMIT_PRIORITY_POLLING_REGISTER_NORMAL_PRIORITY_QUEUE_POLLING_BIT;
+	
+	// Wait for the packet to be transmitted
+	while (Ethernet_Controller_Transmit_Descriptor.Flags & (1 << ETHERNET_CONTROLLER_TRANSMIT_DESCRIPTOR_OWNERSHIP_BIT)); // The owner bit is automatically cleared when the packet is transmitted
 }
 
 int EthernetIsPacketReceived(void)
