417255fde7b1f484b07ca2098efe9c9c3ff9e8e2
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2016-11-03 11:52:49 +0100

    Added 'put' command to UNIX commands tftp.

--- Applications/Sources/UNIX_Commands/Command_TFTP.c
+++ Applications/Sources/UNIX_Commands/Command_TFTP.c
@@ -9,14 +9,20 @@
 //-------------------------------------------------------------------------------------------------
 // Private variables
 //-------------------------------------------------------------------------------------------------
+/** The data transfer mode for read and write requests. */
+#define STRING_TFTP_TRANSFER_MODE "octet"
+
+//-------------------------------------------------------------------------------------------------
+// Private variables
+//-------------------------------------------------------------------------------------------------
 /** The socket used to communicate with the server. */
 static TNetworkSocket Socket_Server;
 
 //-------------------------------------------------------------------------------------------------
 // Private functions
 //-------------------------------------------------------------------------------------------------
-/** Get a file from the remote server.
- * @param String_File_Name The remove file name.
+/** Receive a file from the remote server.
+ * @param String_File_Name The remote file name.
  * @return 0 if the file was successfully retrieved,
  * @return 1 if an error occurred.
  */
@@ -24,7 +30,6 @@ int TFTPExecuteCommandGet(char *String_File_Name)
 {
 	TNetworkTFTPPacket Packet;
 	unsigned int File_Name_Length, Transfer_Mode_Length, File_ID, Data_Size;
-	char *String_Transfer_Mode = "octet";
 	int Return_Value = 1;
 	unsigned short Received_Block_Number, Expected_Block_Number = 1;
 	
@@ -34,13 +39,13 @@ int TFTPExecuteCommandGet(char *String_File_Name)
 	StringCopyUpToNumber(String_File_Name, Packet.Request.String_File_Name_And_Mode, FILE_NAME_LENGTH);
 	File_Name_Length = StringGetSize(Packet.Request.String_File_Name_And_Mode);
 	// Append the transfer mode
-	StringCopy(String_Transfer_Mode, &Packet.Request.String_File_Name_And_Mode[File_Name_Length + 1]); // Append the string right after the file name string terminating zero
-	Transfer_Mode_Length = StringGetSize(String_Transfer_Mode);
+	StringCopy(STRING_TFTP_TRANSFER_MODE, &Packet.Request.String_File_Name_And_Mode[File_Name_Length + 1]); // Append the string right after the file name string terminating zero
+	Transfer_Mode_Length = StringGetSize(STRING_TFTP_TRANSFER_MODE);
 	
 	// Open the file to be ready to write it's content
 	if (FileOpen(String_File_Name, FILE_OPENING_MODE_WRITE, &File_ID) != 0)
 	{
-		ScreenWriteString(STRING_COMMAND_TFTP_CANT_OPEN_FILE);
+		ScreenWriteString(STRING_COMMAND_TFTP_GET_CANT_OPEN_FILE);
 		return 1; // Do not go through Exit label path or a file with the same name than the one that would be opened could be deleted
 	}
 	
@@ -48,13 +53,13 @@ int TFTPExecuteCommandGet(char *String_File_Name)
 	if (NetworkUDPSendBuffer(&Socket_Server, sizeof(Packet.Opcode) + File_Name_Length + 1 + Transfer_Mode_Length + 1, &Packet) != 0) // +2 bytes for both strings terminating zeroes
 	{
 		ScreenWriteString(STRING_COMMAND_TFTP_NETWORK_TRANSMISSION_FAILED);
-		return -1;
+		goto Exit;
 	}
 	
 	ScreenWriteString(STRING_COMMAND_TFTP_STARTING_DOWNLOAD);
 	
 	// Receive the file
-	while (1)
+	do
 	{
 		// Get a packet
 		if (NetworkTFTPReceivePacket(&Socket_Server, 1000, &Data_Size, &Packet) != 0) // Data_Size contains for now the raw TFTP packet size
@@ -84,11 +89,11 @@ int TFTPExecuteCommandGet(char *String_File_Name)
 		Received_Block_Number = NETWORK_SWAP_WORD(Packet.Data.Block_Number);
 		if (Received_Block_Number != Expected_Block_Number)
 		{
-			ScreenWriteString(STRING_COMMAND_TFTP_BAD_BLOCK_NUMBER_1);
+			ScreenWriteString(STRING_COMMAND_TFTP_GET_BAD_BLOCK_NUMBER_1);
 			ScreenWriteUnsignedInteger(Received_Block_Number);
-			ScreenWriteString(STRING_COMMAND_TFTP_BAD_BLOCK_NUMBER_2);
+			ScreenWriteString(STRING_COMMAND_TFTP_GET_BAD_BLOCK_NUMBER_2);
 			ScreenWriteUnsignedInteger(Expected_Block_Number);
-			ScreenWriteString(STRING_COMMAND_TFTP_BAD_BLOCK_NUMBER_3);
+			ScreenWriteString(STRING_COMMAND_TFTP_GET_BAD_BLOCK_NUMBER_3);
 			goto Exit;
 		}
 		Expected_Block_Number++;
@@ -110,10 +115,7 @@ int TFTPExecuteCommandGet(char *String_File_Name)
 			ScreenWriteString(STRING_COMMAND_TFTP_NETWORK_TRANSMISSION_FAILED);
 			goto Exit;
 		}
-		
-		// Exit if the data size is different from a block size
-		if (Data_Size != NETWORK_TFTP_BLOCK_SIZE) break;
-	}
+	} while (Data_Size == NETWORK_TFTP_BLOCK_SIZE); // Exit if the data size is different from a block size
 	
 	// Display a success message
 	ScreenSetFontColor(SCREEN_COLOR_GREEN);
@@ -126,6 +128,114 @@ int TFTPExecuteCommandGet(char *String_File_Name)
 Exit:
 	FileClose(File_ID);
 	if (Return_Value != 0) FileDelete(String_File_Name); // Remove the partial file
+	return Return_Value;
+}
+
+/** Send a file to the remote server.
+ * @param String_File_Name The local file name.
+ * @return 0 if the file was successfully sent,
+ * @return 1 if an error occurred.
+ */
+int TFTPExecuteCommandPut(char *String_File_Name)
+{
+	TNetworkTFTPPacket Packet;
+	unsigned int File_ID, File_Name_Length, Transfer_Mode_Length, Received_Block_Number, Sent_Block_Number = 0, Read_Bytes_Count, Size;
+	int Return_Value = 1;
+	
+	// Try to open the local file
+	if (FileOpen(String_File_Name, FILE_OPENING_MODE_READ, &File_ID) != 0)
+	{
+		ScreenWriteString(STRING_COMMAND_TFTP_PUT_CANT_OPEN_FILE);
+		goto Exit;
+	}
+	
+	// Prepare the write request
+	Packet.Opcode = NETWORK_SWAP_WORD(NETWORK_TFTP_OPCODE_WRITE_REQUEST);
+	// Append the sent file name
+	StringCopyUpToNumber(String_File_Name, Packet.Request.String_File_Name_And_Mode, FILE_NAME_LENGTH);
+	File_Name_Length = StringGetSize(Packet.Request.String_File_Name_And_Mode);
+	// Append the transfer mode
+	StringCopy(STRING_TFTP_TRANSFER_MODE, &Packet.Request.String_File_Name_And_Mode[File_Name_Length + 1]); // Append the string right after the file name string terminating zero
+	Transfer_Mode_Length = StringGetSize(STRING_TFTP_TRANSFER_MODE);
+	
+	// Send the write request
+	if (NetworkUDPSendBuffer(&Socket_Server, sizeof(Packet.Opcode) + File_Name_Length + 1 + Transfer_Mode_Length + 1, &Packet) != 0) // +2 bytes for both strings terminating zeroes
+	{
+		ScreenWriteString(STRING_COMMAND_TFTP_NETWORK_TRANSMISSION_FAILED);
+		goto Exit;
+	}
+	
+	ScreenWriteString(STRING_COMMAND_TFTP_STARTING_UPLOAD);
+	
+	// Receive the file
+	do
+	{
+		// Get a packet
+		if (NetworkTFTPReceivePacket(&Socket_Server, 1000, &Size, &Packet) != 0)
+		{
+			ScreenWriteString(STRING_COMMAND_TFTP_NETWORK_RECEPTION_FAILED);
+			goto Exit;
+		}
+		Packet.Opcode = NETWORK_SWAP_WORD(Packet.Opcode);
+		
+		// Did an error occurred ?
+		if (Packet.Opcode == NETWORK_TFTP_OPCODE_ERROR)
+		{
+			ScreenWriteString(STRING_COMMAND_TFTP_NETWORK_SERVER_ERROR_1);
+			ScreenWriteString(Packet.Error.String_Error_Message);
+			ScreenWriteString(STRING_COMMAND_TFTP_NETWORK_SERVER_ERROR_2);
+			goto Exit;
+		}
+		
+		// Is it an ACK packet ?
+		if (Packet.Opcode != NETWORK_TFTP_OPCODE_ACKNOWLEDGMENT)
+		{
+			ScreenWriteString(STRING_COMMAND_TFTP_NETWORK_BAD_PACKET_RECEIVED);
+			goto Exit;
+		}
+		
+		// Make sure the acknowledged block number is the good one
+		Received_Block_Number = NETWORK_SWAP_WORD(Packet.Acknowledgment.Block_Number);
+		if (Received_Block_Number != Sent_Block_Number)
+		{
+			ScreenWriteString(STRING_COMMAND_TFTP_PUT_BAD_BLOCK_NUMBER_1);
+			ScreenWriteUnsignedInteger(Received_Block_Number);
+			ScreenWriteString(STRING_COMMAND_TFTP_PUT_BAD_BLOCK_NUMBER_2);
+			ScreenWriteUnsignedInteger(Sent_Block_Number);
+			ScreenWriteString(STRING_COMMAND_TFTP_PUT_BAD_BLOCK_NUMBER_3);
+			goto Exit;
+		}
+		Sent_Block_Number++;
+		
+		// Read the next block from the file
+		if (FileRead(File_ID, Packet.Data.Buffer, NETWORK_TFTP_BLOCK_SIZE, &Read_Bytes_Count) != 0)
+		{
+			ScreenWriteString(STRING_COMMAND_TFTP_PUT_CANT_READ_FROM_FILE);
+			goto Exit;
+		}
+		
+		// Prepare the DATA packet
+		Packet.Opcode = NETWORK_SWAP_WORD(NETWORK_TFTP_OPCODE_DATA);
+		Packet.Data.Block_Number = NETWORK_SWAP_WORD(Sent_Block_Number);
+		
+		// Send the packet (even if the read data was 0 byte, which will tell the server that the transfer is terminated)
+		if (NetworkUDPSendBuffer(&Socket_Server, sizeof(Packet.Opcode) + sizeof(Packet.Data.Block_Number) + Read_Bytes_Count, &Packet) != 0)
+		{
+			ScreenWriteString(STRING_COMMAND_TFTP_NETWORK_TRANSMISSION_FAILED);
+			goto Exit;
+		}
+	} while (Read_Bytes_Count == NETWORK_TFTP_BLOCK_SIZE); // Exit when the whole file has been sent
+	
+	// Display a success message
+	ScreenSetFontColor(SCREEN_COLOR_GREEN);
+	ScreenWriteString(STRING_COMMAND_TFTP_PUT_UPLOAD_SUCCESSFUL_1);
+	ScreenWriteUnsignedInteger(Sent_Block_Number);
+	ScreenWriteString(STRING_COMMAND_TFTP_PUT_UPLOAD_SUCCESSFUL_2);
+	ScreenSetFontColor(SCREEN_COLOR_BLUE);
+	Return_Value = 0;
+	
+Exit:
+	FileClose(File_ID);
 	return Return_Value;
 }
 
@@ -186,7 +296,7 @@ int CommandMainTFTP(int argc, char __attribute__((unused)) *argv[])
 	
 	// Execute the requested command
 	if (Is_Get_Command) return TFTPExecuteCommandGet(String_File_Name);
-	//else TODO
+	else TFTPExecuteCommandPut(String_File_Name);
 	
 	return 0;
 }
