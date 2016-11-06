170f19d2c2c9a6336c2e9676f2c58d6030f48347
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2016-11-06 11:23:37 +0100

    Made Add_Executable_Header.sh Applications tool compatible with Debian dash, the default /bin/sh on Debian.

--- Applications/Tools/Add_Executable_Header.sh
+++ Applications/Tools/Add_Executable_Header.sh
@@ -3,8 +3,8 @@
 INPUT_FILE=$1
 OUTPUT_FILE=$2
 
-# Add executable magic number
-echo -ne "\x12\x34\x56\x78" > $OUTPUT_FILE
+# Add executable magic number (octal values and printf are used to be compatible with default Debian /bin/sh, which is the prehistorical dash)
+printf "\022\064\126\170" > $OUTPUT_FILE
 
 # Concatenate the remaining part of the binary
 cat $INPUT_FILE >> $OUTPUT_FILE
