b613abf527472bde1d6388046e47a586949aabc3
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2019-08-25 15:36:01 +0200

    Removed Readme.txt resource file as the information it contained are now displayed by Help application.

--- Applications/Resources/Readme.txt
+++
@@ -1,6 +0,0 @@
-Bienvenue sur le systŠme d'exploitation Lemon, un systŠme minimaliste
-et monotƒche ax‚ sur la simplicit‚ et la vitesse d'ex‚cution.
-
-Ce document ne contient pour l'instant qu'une seule information utile :
-Vous pouvez appuyer … tout moment sur la touche "F12" pour interrompre la
-tƒche en cours d'ex‚cution et revenir au systŠme.
--- Configurations/Template_Installer_Files_List_Network_Disabled.mk
+++ Configurations/Template_Installer_Files_List_Network_Disabled.mk
@@ -1,1 +1,1 @@
-SYSTEM_INSTALLER_FILES_LIST = "Applications/Binaries/benchmark Applications/Binaries/edit Applications/Binaries/games Applications/Binaries/help Applications/Resources/Readme.txt Applications/Binaries/tests Applications/Binaries/u"
+SYSTEM_INSTALLER_FILES_LIST = "Applications/Binaries/benchmark Applications/Binaries/edit Applications/Binaries/games Applications/Binaries/help Applications/Binaries/tests Applications/Binaries/u"
